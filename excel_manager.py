a76f46822cb46090c6ff1e0e4c6509116efd19bf
NLynch19 <neil.f.lynch@gmail.com>
2025-09-27 12:11:57 +0400

    Update excel_manager.py

--- excel_manager.py
+++ excel_manager.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+def load_excel(path, columns):
+    try:
+        return pd.read_excel(path)
+    except FileNotFoundError:
+        return pd.DataFrame(columns=columns)
+
+def save_to_excel(df, path):
+    df.to_excel(path, index=False)
 
